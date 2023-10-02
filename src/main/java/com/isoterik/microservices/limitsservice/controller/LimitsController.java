d3be2e5b45d6c69ace5b9533e8ba5d6004f0da94
imran-laborjack <isoteriksoftware@gmail.com>
2023-10-02 09:59:38 +0100

    update

---
+++ src/main/java/com/isoterik/microservices/limitsservice/controller/LimitsController.java
@@ -0,0 +1,20 @@
+package com.isoterik.microservices.limitsservice.controller;
+
+import com.isoterik.microservices.limitsservice.bean.Limits;
+import com.isoterik.microservices.limitsservice.configuration.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LimitsController {
+    private final Configuration configuration;
+
+    public LimitsController(Configuration configuration) {
+        this.configuration = configuration;
+    }
+
+    @GetMapping("/limits")
+    public Limits retrieveLimits() {
+        return new Limits(configuration.getMinimum(), configuration.getMaximum());
+    }
+}
